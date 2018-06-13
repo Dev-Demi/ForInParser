4d140c1f8f907ec6f030d27295f736f4ea773089
Dev-Demi <BoxDemi@gmail.com>
2018-06-13 11:33:28 +0300

    Initial commit

---
+++ Demo/Unit1.pas
@@ -0,0 +1,39 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls,UForInParser;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+ s:string;
+begin
+ for s in StringParser('11,22,33,44,55',',') do
+  Memo1.Lines.add(s);
+ for s in StringParser('one two three four five',' ') do
+  Memo1.Lines.add(s);
+ for s in StringParser('Test1;Test2;Test3;Test4;Test5',';') do
+  Memo1.Lines.add(s);
+end;
+
+end.
---
+++ Source/UForInParser.pas
@@ -0,0 +1,101 @@
+unit UForInParser;
+
+interface
+
+{$IF Defined(FPC) or (COMPILERVERSION>=17)}
+{$DEFINE SUPPORTS_INLINE}
+{$DEFINE SUPPORTS_FOR_IN}
+{$IFEND}
+
+uses System.SysUtils;
+
+{$IFDEF SUPPORTS_FOR_IN}
+
+Type
+  TParserEnumerator = class
+  private
+    CurString:String;
+    SourceString, DelimiterChar: string;
+    function GetCurrentString: string;
+  public
+    // обслуживание перечислителя, не для внешнего использования!
+    property Current: string read GetCurrentString;
+    function MoveNext: boolean;
+    function GetEnumerator: TParserEnumerator; {$IFDEF SUPPORTS_INLINE}inline;
+{$ENDIF}
+    // генерировать перечень файлов
+    Constructor Create(const Source: string; Delimiter: Char);
+    Destructor Destroy; override;
+  end;
+///   <summary>
+///   Возвращает из строки Source подстроки разделенные символом Delimiter
+///   Returns from string Source substrings separated by Delimiter
+///   </summary>
+
+function StringParser(const Source: string; Delimiter: Char): TParserEnumerator;
+{$ENDIF}
+
+implementation
+
+{$IFDEF SUPPORTS_FOR_IN}
+
+function TParserEnumerator.GetCurrentString: string;
+begin
+ result:=CurString;
+end;
+
+function TParserEnumerator.MoveNext: boolean;
+var
+ i,LengthStr:integer;
+
+begin
+ i:=1;
+ LengthStr:=Length(SourceString);
+
+ if LengthStr=0 then
+ begin
+  Result:=false;
+  exit;
+ end;
+
+ while (SourceString[i]<>DelimiterChar)and(i<LengthStr) do
+  i:=i+1;
+
+ result:= i<=Length(SourceString);
+
+ if Result then
+ begin
+ if i=Length(SourceString) then
+  CurString:=copy(SourceString,1,i)
+ else
+  CurString:=copy(SourceString,1,i-1);
+  delete(SourceString,1,i);
+ end;
+end;
+
+function TParserEnumerator.GetEnumerator: TParserEnumerator;
+begin
+  Result := Self;
+end;
+
+Constructor TParserEnumerator.Create(const Source: string; Delimiter: Char);
+begin
+  inherited Create;
+
+  SourceString:=Source;
+  DelimiterChar:=Delimiter
+end;
+
+Destructor TParserEnumerator.Destroy;
+begin
+  inherited;
+end;
+
+function StringParser(const Source: string; delimiter: Char): TParserEnumerator;
+begin
+  Result := TParserEnumerator.Create(Source, delimiter);
+end;
+
+{$ENDIF}
+
+end.
